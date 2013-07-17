90e38d39685654152ba8cc40713da3ae3757df61
Raphael Rosenberg <startupeng@stanford.edu>
2013-07-17 03:17:22 +0000

    Adding file reading capability to web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-  var output = fs.readFileSync(index.html);
+  var output = fs.readFileSync('index.html');
   response.send(output.toString());
 });
 
